ca4452a91fc4b3bbfb90c8d76742744d996f503d
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-09-27 04:05:57 +0100

    create spot routes

--- src/routes/spot.js
+++ src/routes/spot.js
@@ -1,9 +1,18 @@
-const router = require(express).Router
-const Spot = require('../models/spot')
+const express = require('express')
+// --------------------------------
+const { getSpots, getSpot, newSpot } = require('../controllers/spot')
 
 /** Spot routes */
+const router = express.Router()
 
 // get all spots
 router.get('/', getSpots)
+
+// get single spot by id
+router.get('/:id', getSpot)
+
+// render new spot form 
+router.get('/new', newSpot)
+
 
 module.exports = router
