14a62c0237005b2e3aef838b58828be31c66808d
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-09-30 21:08:54 +0100

    add database query result to response object

--- src/controllers/spot.js
+++ src/controllers/spot.js
@@ -3,15 +3,23 @@ const Spot = require('../models/spot')
 exports.getSpots = (req, res) => {
   const css = "/styles/spots/index.css"
   Spot.findAll()
-    .then(spots => res.render('./spots/index', {spots, css}))
+    .then(spots => {
+      // console.log(spots)
+      res.render('./spots/index', {spots, css})
+    })
     .catch(err => res.status(404).json({message: err.message}))
 }
 
 exports.getSpot = (req, res) => {
    const css = "/styles/spots/show.css"
    const { id } = req.params
+   let error
    Spot.findByPk(id)
-    .then(spot => res.render('./spots/show', {spot, css}))
+    .then(spot => {
+      // console.log(spot.name)
+      error = spot == null ? false : true
+      res.render('./spots/show', {spot, css, error})
+    })
     .catch(err => res.status(404).json({message: err.message}))
 }
 
