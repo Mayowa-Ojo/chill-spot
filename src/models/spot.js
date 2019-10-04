ab2736f030c8665a5957fbd79842d00de83ee93f
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-10-04 23:03:29 +0100

    add unique id and category fields to spot schema

--- src/models/spot.js
+++ src/models/spot.js
@@ -1,8 +1,15 @@
-const { STRING, DATE } = require('sequelize')
+const { STRING, DATE, UUID } = require('sequelize')
+const uuid = require('uuid/v4')
+/** --------------------------------------------- */
 const { sequelize } = require('../config/sequelize')
 
 /** define a model for a chill spot */
 const Spot = sequelize.define('spot', {
+  id: {
+    type: UUID,
+    allowNull: false,
+    primaryKey: true
+  },
   name: {
     type: STRING,
     allowNull: false
@@ -22,10 +29,26 @@ const Spot = sequelize.define('spot', {
   price_range: {
     type: STRING,
     allowNull: false
+  },
+  category: {
+    type: STRING,
+    allowNull: true
   }
 },
 {
   timestamps: false
 })
+
+// create a model hook to set the id to a unique value
+Spot.beforeValidate(function(spot, options) {
+  return new Promise((resolve, reject) => {
+    spot.id = uuid()
+    return resolve(spot, options)
+  })
+})
+
+// Spot.addHook('beforeValidate', (spot, options) => {
+//   spot.id = uuid()
+// })
 
 module.exports = Spot
