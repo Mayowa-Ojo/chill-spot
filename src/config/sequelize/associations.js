6e3c90eafade433b2f5d46723ed478d5c24cb85a
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-10-06 12:39:16 +0100

    implement sequelize associations config

---
+++ src/config/sequelize/associations.js
@@ -0,0 +1,22 @@
+const { sequelize } = require('./sequelize')
+const Spot = require('../../models/spot')
+const Comment = require('../../models/comment')
+
+Spot.hasMany(Comment, { foreignKey: 'spotId_fk' })
+Comment.belongsTo(Spot, { foreignKey: 'spotId_fk' })
+
+// sync database
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Synchronizing database...3..2..1...done')
+    // Slug.create({ content: 'test slug...', id: '1', _fk: '1' })
+    // Comment.create({ id: '1', content: 'test comment...', slugId_fk: '1' })
+    // Comment.create({ id: '2', content: 'test comment 2...', slugId_fk: '1' })
+  })
+  .catch(err => console.log(err))
+
+
+module.exports = {
+  Spot,
+  Comment
+}
--- src/config/sequelize/sequelize.js
+++ src/config/sequelize/sequelize.js
@@ -11,6 +11,7 @@ const sequelize = new Sequelize(PG_DATABASE, PG_USERNAME, PG_PASSWORD, {
   dialect: 'postgres'
 })
 
+// test connection
 sequelize
   .authenticate()
   .then(() => {
@@ -19,5 +20,8 @@ sequelize
   .catch(err => {
     console.error('connection to sql database failed ', err)
   })
-  
+
+// // sync database
+// sequelize.sync({ force: true })
+
 module.exports = { sequelize }
