ac2a0e49f93d6e5c0c8b189df9c6430d880e0eb9
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-10-06 12:40:14 +0100

    update comments route

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,10 +1,12 @@
 const express = require('express')
 // ******************************
-const { getComments, createComment, editComment, deleteComment } = require('../controllers/comment')
+const { getComments, getComment, createComment, editComment, deleteComment } = require('../controllers/comment')
 const router = express.Router()
 
 /** comment routes */
 router.get('/', getComments)
+
+router.get('/:id', getComment)
 
 router.post('/', createComment)
 
