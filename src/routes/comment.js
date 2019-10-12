f46d40c7ec0e8e51dea80e70f7556c6de6ffd2c3
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-10-12 16:04:57 +0100

    feat(routes): authorize protected routes

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,18 +1,21 @@
 const express = require('express')
 // ******************************
 const { getComments, getComment, createComment, editComment, deleteComment, likeComment } = require('../controllers/comment')
+const { getComment: getCommentMiddleware } = require('../middlewares/comment')
+const { authorizeUser } = require('../middlewares/auth')
+
 const router = express.Router()
 
 /** comment routes */
 router.get('/', getComments)
 
-router.get('/:id', getComment)
+router.get('/:id', getCommentMiddleware, getComment)
 
 router.post('/', createComment)
 
-router.put('/:id/edit', editComment)
+router.put('/:id/edit', getCommentMiddleware, authorizeUser, editComment)
 
-router.get('/:id/delete', deleteComment)
+router.get('/:id/delete', getCommentMiddleware, authorizeUser, deleteComment)
 
 router.get('/:id/like', likeComment)
 
