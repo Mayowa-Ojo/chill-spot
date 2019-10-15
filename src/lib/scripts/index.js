e93ed9efa4e9992fba1fa8ac46a72e6b075ca604
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-10-15 19:14:43 +0100

    feat(script): implement vertical menu ui functionality

--- src/lib/scripts/index.js
+++ src/lib/scripts/index.js
@@ -1,48 +1,25 @@
-/* ********************************************************************************************
-    Hanlde comment editing - Including swapping the comment element with text input form
-******************************************************************************************** */
-const editButtons = $('.actions .edit.outline.icon')
-const buttons = [...editButtons]
+/**********************************
+  script for the index page
+  make the vertical menu interactive
+**********************************/
 
-const handleClick = (e) => {
-  // select target element with class identifier
-  const identifier = e.target.parentElement.parentElement.parentElement.children[2].classList[1]
-  const content = e.target.parentElement.parentElement.parentElement.children[2].textContent
-  const commentId = [...$('.content input[type=\'hidden\']')][identifier].value
-  // replacement element
-  const elem = `
-  <form action="/comments/${commentId}/edit?_method=PUT" method="POST">
-    <div class="ui mini fluid icon action input">
-      <input type="text" name="comment" value="${content}">
-      <button type="submit" class="ui icon mini button">
-        <i class="check circle icon"></i>
-      </button>
-    </div>
-  </form>
-  `
-  // replace comment with text input
-  $(elem).replaceAll(`.text.${identifier}`)
+// select links in the vertical menu 
+const links = [...$('.ui.vertical.menu a')]
 
-  /*
-  add event listener to submit button
-  const submitButton = [...$('.ui.icon.input .ui.icon.button')][0]
-  submitButton.addEventListener('click', (e) => {
-    let inputValue
-    
-    if([...e.target.classList].includes('check')) {
-      inputValue = e.target.parentElement.parentElement.children[0].value
-    } else {
-      inputValue = e.target.parentElement.children[0].value
+// add listener(click) to each link
+links.forEach(link => {
+  link.addEventListener('click', toggleActiveLink)
+})
+
+function toggleActiveLink(e) {
+  // remove the current active class
+  links.forEach(link => {
+    if(link.classList.contains('active')) {
+      link.classList.remove('active', 'teal')
     }
-    
-    // assign replacement element with text content set to input value
-    const replacement = `<div class="text ${identifier}">${inputValue}</div>`
-    $(replacement).replaceAll('.ui.fluid.icon.input')
   })
-  */
+  // add active class to target link
+  e.target.classList.add('active', 'teal')
 }
 
-// add event listner to all edit buttons
-buttons.forEach(button => {
-  button.addEventListener('click', handleClick)
-})
+// console.log(links)
